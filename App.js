205ac001ef3353e766b99a5f9672c5de8dab720a
xinpengliu <daaavidlxp@hotmail.com>
2022-05-13 12:25:50 -0400

    Added basic code to the proejct template, also added screens

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import ListScreen from './src/screens/ListScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    List: ListScreen,
   },
-});
+  {
+    initialRouteName: 'List',
+    defaultNavigationOptions: {
+      title: 'FoodiesHub'
+    }
+  }
+);
+
+export default createAppContainer(navigator);

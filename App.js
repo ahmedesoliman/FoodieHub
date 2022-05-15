9eac271ae9f24744cc3edad7a84d822ccd6623ec
xinpengliu <daaavidlxp@hotmail.com>
2022-05-15 16:29:07 -0400

    Added template account and map screen for building navigation bar

--- App.js
+++ App.js
@@ -3,16 +3,19 @@ import { createAppContainer } from "react-navigation";
 import LoginScreen from './src/screens/LoginScreen';
 import ListScreen from "./src/screens/ListScreen";
 import DetailScreen from "./src/screens/DetailScreen";
-
+import MapScreen from "./src/screens/MapScreen";
+import AccountScreen from "./src/screens/AccountScreen";
 
 const navigator = createStackNavigator(
   {
     Login: LoginScreen,
     List: ListScreen,
-    Detail: DetailScreen
+    Detail: DetailScreen,
+    Map: MapScreen,
+    Account: AccountScreen
   },
   {
-    initialRouteName: 'List',
+    initialRouteName: 'Login',
     defaultNavigationOptions: {
       title: 'StreetFood'
     }

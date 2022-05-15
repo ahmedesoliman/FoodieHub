9f8d2a8faafe0ab5ae03b5ed3b10f6dcb8912548
Min Shin Khant <minshinkhant7@gmail.com>
2022-05-15 02:18:46 -0400

    adding forget_password_screen and register_screen with firebase

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import ListScreen from './src/screens/ListScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from "./src/screens/RegisterScreen";
+import ForgetPasswordScreen from "./src/screens/ForgetPassword";
 
 
 const navigator = createStackNavigator(
   {
     login: LoginScreen,
-    list:ListScreen
+    list:ListScreen,
+    register:RegisterScreen,
+    forgot_password: ForgetPasswordScreen
   },
   {
     initialRouteName: 'login',
